842508bd1b6e7335b41732ddedce768e1d0a2510
zoeBryan359 <zoebryan@whis.k12.hi.us>
2022-04-28 17:46:00 -1200

    added txt

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        int teamNumber = 694;
+        String teamName = "StuyPulse";
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.println(teamName);
+        System.out.println(teamNumber);
+
+        
+
+    }
+}
